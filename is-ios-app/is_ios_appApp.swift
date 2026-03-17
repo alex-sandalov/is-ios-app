755082ed4180f32d049d31686a03c39cb26421c1
alex-sandalov <sanyok.sandalov@gmail.com>
2026-03-18 01:32:33 +0300

    Initial Commit

---
+++ is-ios-app/is_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  is_ios_appApp.swift
+//  is-ios-app
+//
+//  Created by Alexander on 18.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct is_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ is-ios-appTests/is_ios_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  is_ios_appTests.swift
+//  is-ios-appTests
+//
+//  Created by Alexander on 18.03.2026.
+//
+
+import Testing
+@testable import is_ios_app
+
+struct is_ios_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
